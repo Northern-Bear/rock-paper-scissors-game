1ba9b0f1a1ddd98273068cf1186720eed582ec05
Eric Bear <eric.lj.bear@gmail.com>
2026-08-15 21:26:02 -0600

    feat: add pseudocode planning

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("Hello, World!");
+// **1. Get the computers choice:
+//      - Create a function called getComputerChoice.
+//      - Generate a random number.
+//      - Map the number to 'Rock', 'Paper', or 'Scissors'.
+//      - Return that string.
+// **2. Get the humans choice:
+//      - Create function called getHumanChoice.
+//      - Prompt user to type 'Rock', 'Paper', or 'Scissors'.
+//      - Return the users input.
+// **3. Keep track of the scores:
+//      - Create variables for humanScore and computerScore.
+//      - Initialize both to 0.
+// **4. Play a single round:
+//      - Create function called playRound that takes getComputerChoice and getHumanChoice as arguments.
+//      - Make the choices case-sensitive (so 'rock' or 'ROCK' both work).
+//      - Compare the choices using conditional logic (if/else statements).
+//      - Log the winner message to the console (e.g. "You Lost! Paper beats Rock").
+//      - Increment the winners score variable
+// **5. Play the full game:
+//      - Create function called playGame.
+//      - Call playRound inside it 5 times.
+//      - Compare final scores and declare the winner.
