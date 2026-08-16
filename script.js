ea3dc0ccbaa1cd5345604c4ea1266f4e409b52a5
Eric Bear <eric.lj.bear@gmail.com>
2026-08-15 23:08:29 -0600

    feat: implement random computer choice function

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 // **1. Get the computers choice:
 //      - Create a function called getComputerChoice.
 //      - Generate a random number.
 //      - Map the number to 'Rock', 'Paper', or 'Scissors'.
 //      - Return that string.
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+function getComputerChoice() {
+  let randomNum = Math.floor(Math.random() * 3) + 1;
+  let choice = "";
+
+  switch (randomNum) {
+    case 1:
+      choice = "Rock";
+      break;
+    case 2:
+      choice = "Paper";
+      break;
+    case 3:
+      choice = "Scissors";
+      break;
+    default:
+      choice = "Unknown";
+  }
+
+  return choice;
+}
+
 // **2. Get the humans choice:
 //      - Create function called getHumanChoice.
 //      - Prompt user to type 'Rock', 'Paper', or 'Scissors'.
